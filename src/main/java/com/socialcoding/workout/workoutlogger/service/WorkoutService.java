bf9eee5dab03ccbb86f1f26ccddeb47ce2a9c709
rleeap <rekaml@live.com>
2019-11-21 19:14:40 -0600

    wrote WorkoutService

---
+++ src/main/java/com/socialcoding/workout/workoutlogger/service/WorkoutService.java
@@ -0,0 +1,17 @@
+package com.socialcoding.workout.workoutlogger.service;
+
+import com.socialcoding.workout.workoutlogger.entity.Workout;
+
+import java.util.List;
+
+public interface WorkoutService {
+
+        Workout findWorkoutById(int id);
+
+        void saveWorkout(Workout workout);
+
+        void deleteWorkoutById(int id);
+
+        List<Workout> findAllWorkout();
+
+}
